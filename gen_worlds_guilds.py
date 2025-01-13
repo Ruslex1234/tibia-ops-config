75cabb247c3a245c7d74fb6d900af3991ba95c5e
Ruslex1234 <thero1234@hotmail.com>
2025-01-13 12:46:40 -0800

    Update gen_worlds_guilds.py

--- gen_worlds_guilds.py
+++ gen_worlds_guilds.py
@@ -5,7 +5,7 @@ import gzip
 from io import BytesIO
 
 # Worlds to fetch guilds from
-WORLDS = ['Quidera', 'Vandera', 'Flamera', 'Temera', 'Fibera', 'Firmera', 'Aethera', 'Gravitera', 'Wildera', 'Talera', 'Lobera', 'Quintera']
+WORLDS = ['Quidera', 'Vandera', 'Flamera', 'Temera', 'Fibera', 'Firmera', 'Aethera', 'Gravitera', 'Wildera', 'Talera', 'Lobera', 'Quintera', 'Wintera']
 
 def fetch_guilds_for_world(world):
     url = f"https://api.tibiadata.com/v4/guilds/{world}"
